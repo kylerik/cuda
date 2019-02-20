52a57f85d68ad4856bd5c6b94517a217e2251396
eriklachapelle <46822410+eriklachapelle@users.noreply.github.com>
2019-02-20 18:55:01 -0500

    data structure v1

---
+++ js/main.js
@@ -0,0 +1,93 @@
+// OBJECTS //
+
+let classA = {
+  class1: `Semiotics`,
+  class2: `3-D Vis 2`,
+  class3: `Immersive Environments`,
+  class4: `Interface Dev`
+};
+
+let priority = {
+  low: `low`,
+  med: `medium`,
+  high: `high`
+};
+
+let grade = {
+  grade5: `5%`,
+  grade1: `10%`,
+  grade15: `15%`,
+  grade2: `20%`,
+  grade25: `25%`,
+  grade3: `30%`,
+  grade35: `35%`,
+  grade4: `40%`,
+  grade45: `45%`,
+  grade50: `50%`
+};
+
+var complete = {
+  none: `none`,
+  just: `Just started`,
+  half: `Half way`,
+  almost: `Almost done`
+};
+
+let week = {
+  week1: `1`,
+  week2: `2`,
+  week3: `3`,
+  week4: `4`,
+  week5: `5`,
+  week6: `6`,
+  week7: `7`,
+  week8: `8`,
+  week9: `9`,
+  week10: 10,
+  week11: 11,
+  week12: 12,
+  week13: 13,
+  week14: 14,
+  week15: 15
+};
+
+var assignments = [
+  {
+    class: class3,
+    priority: low,
+    grade: grade15,
+    complete: just,
+    week: week10
+  },
+  {
+    class: class2,
+    priority: high,
+    grade: grade1,
+    complete: half,
+    week: week8
+  }
+];
+
+let assignment3 = {
+  class: class2,
+  priority: med,
+  grade: grade45,
+  complete: just,
+  week: week6
+};
+
+let assignment4 = {
+  class: class1,
+  priority: low,
+  grade: grade15,
+  complete: none,
+  week: week11
+};
+
+let assignment5 = {
+  class: class3,
+  priority: high,
+  grade: grade1,
+  complete: half,
+  week: week8
+};
