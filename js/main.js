dc95c9fe0f417e93d83f8eb261989bf9b5188578
eriklachapelle <46822410+eriklachapelle@users.noreply.github.com>
2019-03-20 09:29:08 -0400

    making the clicks functional

--- js/main.js
+++ js/main.js
@@ -1,35 +1,40 @@
-var assign1 = document.getElementById('assign')
+let assignInput = document.getElementById('assign')
 let newAss = document.getElementById('newAssignment');
 
 
+
  let newAssignment = [
-  { id: 0, name:`#assign1` },
+  { id: 0, name: `who` },
   { id: 1, name: "assign1" },
   { id: 2, name: "finish homework" },
 
 ];
 
 
+ let assign = [
+  { id: 0, input:`blah` },
+  { id: 1, input: "bloo" },
+];
+
 // const tiles = [`assignment1`, `assignment2`, `assignment3`];
 // console.log(tiles);
-//newAssignment.style.visibilty = `hidden`
 
 
 //button functionality
-let btnElement = document.getElementById('newTi');
-btnElement.onclick =  createNewTile;
+//let btnElement = document.getElementById('newTi');
+//btnElement.onclick =  createNewTile;
+document.getElementById('newTi').addEventListener('click', createNewTile)
 
 
 function createNewTile () {
 
   console.log(`blahblah`);
   // creating a new tile
-  newAss.setAttribute('class', 'row2');
-// setting up the array
+  newAss.setAttribute('class', 'row2', );
 
-  let htmlOutput = newAssignment.map(tiles => `<p> ${tiles.name}</p>`).join('');
-  document.getElementById("newAssignment").innerHTML = htmlOutput;
+// setting up the array
+ let htmlOutput = newAssignment.map(tiles => `<input ${tiles.name} />`,`<select>${tiles.name}</select>`).join('');
+ document.getElementById("newAssignment").innerHTML = htmlOutput;
 }
 
-
-createNewTile ()
+//createNewTile ()
